a4daf33b396692ba55152ed3b50ca1a479cf5c90
Johan Bilien <jobi@litl.com>
2011-07-20 14:27:44 -0400

    WIP

--- TestDraw/Canvas.m
+++ TestDraw/Canvas.m
@@ -110,6 +110,14 @@
     }
 }
 
+void normalizeVector(CGPoint *v, CGFloat length)
+{
+    CGFloat d = sqrtf(v->x * v->x + v->y * v->y);
+    
+    v->x = v->x * sqrtf(length) / d;
+    v->y = v->y * sqrtf(length) / d;
+}
+
 - (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
 {
     NSEnumerator *enumerator = [touches objectEnumerator];
@@ -130,22 +138,31 @@
         
         if (path.pointNMinus2.x == -1) {
             path.pointNMinus2 = previous;
-            path.controlPoint2NMinus2 = previous;
             continue;
         }
 
-        /* Normalizing the control point so that it's a distance of 1 from the previous point */
-        /*
-        CGFloat d1 = sqrtf((previous.x - path.previousControlPoint.x)*(previous.x - path.previousControlPoint.x) +
-                          (previous.y - path.previousControlPoint.y)*(previous.y - path.previousControlPoint.y));
-        CGFloat d2 = sqrtf((previous.x - now.x)*(previous.x - now.x) +
-                           (previous.y - now.y)*(previous.y - now.y));*/
-    
-        CGPoint controlPoint1 = { path.pointNMinus2.x + (path.pointNMinus2.x - path.controlPoint2NMinus2.x),
-                                  path.pointNMinus2.y + (path.pointNMinus2.y - path.controlPoint2NMinus2.y) };
-                                                         
-        CGPoint controlPoint2 = { previous.x - (now.x - path.pointNMinus2.x),
-                                  previous.y - (now.y - path.pointNMinus2.y) };
+        
+        CGFloat length = sqrtf((previous.x - path.pointNMinus2.x) * (previous.x - path.pointNMinus2.x) +
+                               (previous.y - path.pointNMinus2.y) * (previous.y - path.pointNMinus2.y)) / 2;
+        
+        CGPoint vect1;
+        
+        if (path.controlPoint2NMinus2.x == -1) {
+            vect1 = CGPointMake(previous.x - path.controlPoint2NMinus2.x,
+                                previous.y - path.controlPoint2NMinus2.y);
+        } else {
+            vect1 = CGPointMake(path.pointNMinus2.x - path.controlPoint2NMinus2.x,
+                                path.pointNMinus2.y - path.controlPoint2NMinus2.y);
+        }
+        
+        normalizeVector(&vect1, length);
+        CGPoint controlPoint1 = { path.pointNMinus2.x + vect1.x,
+                                  path.pointNMinus2.y + vect1.y };
+                                        
+        CGPoint vect2 = CGPointMake(path.pointNMinus2.x - now.x, path.pointNMinus2.y - now.y);
+        normalizeVector(&vect2, length);
+        CGPoint controlPoint2 = { previous.x + vect2.x,
+                                  previous.y + vect2.y };
 
         CGRect rect = CGRectMake(MIN(previous.x, path.pointNMinus2.x) - 20,
                                  MIN(previous.y, path.pointNMinus2.y) - 20,
